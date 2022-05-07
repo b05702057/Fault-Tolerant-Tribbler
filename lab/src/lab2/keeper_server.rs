cbfb3ceccc0e8e2a1e025d300959622c2a9bf059
李季澄 <b05702057@ntu.edu.com>
2022-05-06 20:15:08 -0700

    change update range

--- lab/src/lab2/keeper_server.rs
+++ lab/src/lab2/keeper_server.rs
@@ -263,7 +263,7 @@ impl KeeperServer {
                 let global_max_clock = *keeper_clock_lock;
                 drop(keeper_clock_lock);
 
-                println!("[DEBUGGING] keeper_server's periodic_scan_and_sync: Before syncing clock(global_max_clock = {}) on backends", global_max_clock);
+                // println!("[DEBUGGING] keeper_server's periodic_scan_and_sync: Before syncing clock(global_max_clock = {}) on backends", global_max_clock);
 
                 let (cur_live_back_indices, global_max_clock) = Self::single_scan_and_sync(
                     storage_clients_clones,
@@ -271,10 +271,10 @@ impl KeeperServer {
                     global_max_clock,
                 )
                 .await?;
-                println!(
-                    "[DEBUGGING] keeper_server's periodic_scan: live_addrs.len(): {}",
-                    cur_live_back_indices.len()
-                );
+                // println!(
+                //     "[DEBUGGING] bin_client's periodic_scan: live_addrs.len(): {}",
+                //     cur_live_back_indices.len()
+                // );
 
                 // Update clock reference of keeper
                 let mut k_clock = keeper_clock.write().await;
@@ -600,7 +600,7 @@ impl KeeperServer {
                 // Clone clients since later moving into async for tokio spawn async execution.
                 let storage_clients_clones = clients_for_scanning.clone();
 
-                println!("[DEBUGGING] keeper_server's predecessor scan: Before syncing clock(global_max_clock = {}) on backends", global_max_clock);
+                // println!("[DEBUGGING] keeper_server's predecessor scan: Before syncing clock(global_max_clock = {}) on backends", global_max_clock);
 
                 let (cur_live_back_indices, clock_max_res) = Self::single_scan_and_sync(
                     storage_clients_clones,
@@ -889,9 +889,13 @@ impl KeeperServer {
         drop(ready_sender_opt);
 
         // TESTING
-        let range = latest_monitoring_range_inclusive.read().await;
-        println!("Keeper {}'s range is: {:?}", this, range);
+        let range = latest_monitoring_range_inclusive.write().await;
+        println!("Our range is: {:?} and our index is: {:?}", range, this);
         drop(range);
+
+        let pre_range = predecessor_monitoring_range_inclusive.read().await;
+        println!("Pre range is: {:?} and our index is: {:?}", pre_range, this);
+        drop(pre_range);
 
         let live_backends_view_clone = live_backends_view.clone();
         let storage_clients_clone = storage_clients.clone();
@@ -1163,20 +1167,12 @@ impl KeeperServer {
         let end_positions = end_positions_lock.clone();
         drop(end_positions_lock);
 
-        println!(
-            "[DEBUGGING] update_ranges: end_positions are {:?}",
-            end_positions
-        );
-
         let statuses_lock = statuses.read().await;
         let statuses = statuses_lock.clone();
         drop(statuses_lock);
 
-        println!("[DEBUGGING] update_ranges: stasuses are {:?}", statuses);
-
         // get end positions of alive keepers
         let mut alive_vector = Vec::<u64>::new();
-
         for idx in 0..keeper_addrs.len() {
             if statuses[idx] {
                 alive_vector.push(end_positions[idx]);
@@ -1197,6 +1193,10 @@ impl KeeperServer {
                     let pre_start_idx = ((idx as i32 - 2) + alive_num) % alive_num;
                     let start_position = (alive_vector[start_idx as usize] + 1) % MAX_BACKEND_NUM;
                     let end_position = end_positions[this];
+                    println!(
+                        "this {:?} start position {:?} end position {:?}",
+                        this, start_position, end_position
+                    );
                     if start_position >= back_num as u64
                         && end_position >= back_num as u64
                         && start_position <= end_position
@@ -1207,17 +1207,18 @@ impl KeeperServer {
                     } else if end_position >= back_num as u64 {
                         *latest_monitoring_range_inclusive =
                             Some((start_position as usize, back_num - 1));
-                    } else {
-                        *latest_monitoring_range_inclusive =
-                            Some((start_position as usize, end_position as usize));
                     }
 
                     let pre_start_position =
                         (alive_vector[pre_start_idx as usize] + 1) % MAX_BACKEND_NUM;
                     let pre_end_position = (start_position + MAX_BACKEND_NUM - 1) % MAX_BACKEND_NUM;
+                    println!(
+                        "this {:?} pre start position {:?} pre end position {:?}",
+                        this, pre_start_position, pre_end_position
+                    );
                     if pre_start_position >= back_num as u64
                         && pre_end_position >= back_num as u64
-                        && start_position <= end_position
+                        && pre_start_position <= pre_end_position
                     {
                         *predecessor_monitoring_range_inclusive = None
                     } else if pre_start_position >= back_num as u64 {
@@ -1226,14 +1227,10 @@ impl KeeperServer {
                     } else if pre_end_position >= back_num as u64 {
                         *predecessor_monitoring_range_inclusive =
                             Some((pre_start_position as usize, back_num - 1));
-                    } else {
-                        *predecessor_monitoring_range_inclusive =
-                            Some((pre_start_position as usize, pre_end_position as usize));
                     }
                 }
             }
         }
-        // Important to drop in reverse order as acquire?
         drop(latest_monitoring_range_inclusive);
         drop(predecessor_monitoring_range_inclusive);
         Ok(())
@@ -1299,6 +1296,7 @@ impl KeeperServer {
             }
             current_time = Instant::now(); // reset current time
         }
+
         // update the range
         let _update_result = Self::update_ranges(
             keeper_addrs.clone(),
